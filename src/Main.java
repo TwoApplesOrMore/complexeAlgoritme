43a2a198fac0358d2b617fd67cc0aaa493118b95
twoapplesormore <harryvangastel@hotmail.com>
2017-11-22 14:01:13 +0100

    1.1.2 done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,45 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         new Main().run();
     }
 
+    private Random RNG = new Random();
+    private boolean usedNumber[];
+
     private void run() {
-        System.out.println("Fuck my life");
+        usedArrayPermitation(1000000);
+        //System.out.println(Arrays.toString(usedArrayPermitation(100000)));
+    }
+
+    private int[] usedArrayPermitation(int size) {
+        long startTime = System.nanoTime();
+        usedNumber = new boolean[size];
+        int permitation[] = new int[size];
+        for(int i = 0; i < size; i++) {
+
+            boolean unique = false;
+            while(!unique) {
+                int random = RNG.nextInt(size);
+                if(isUnique(random)) {
+                    permitation[i] = random;
+                    usedNumber[random] = true;
+                    unique = true;
+                }
+            }
+        }
+        long duration = (System.nanoTime() - startTime) / 1000000;
+        System.out.println("Duration: " + duration);
+        return permitation;
+    }
+
+    private boolean isUnique(int random) {
+        if(usedNumber[random]) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
